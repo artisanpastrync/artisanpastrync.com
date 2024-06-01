d1d762b331e2dfd9fc167ff409e67462cdbdd8f4
Hecker <julianheckerdev@gmail.com>
2024-05-31 23:01:01 -0400

    add tailwind primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import type { Config } from 'tailwindcss';
 export default {
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#FFD2D2',
+                },
+            },
+        },
     },
     plugins: [],
 } satisfies Config;
