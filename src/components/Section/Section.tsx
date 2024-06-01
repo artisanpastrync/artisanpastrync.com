1fd4620d5f8d7cdca8ec596393f1d080d76c9038
Hecker <julianheckerdev@gmail.com>
2024-05-31 23:02:51 -0400

    add section compound component

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,28 +1,137 @@
-import { PropsWithChildren, ReactNode } from 'react';
+import Image from 'next/image';
+import { CSSProperties, FC, HTMLAttributes, ReactNode, VideoHTMLAttributes } from 'react';
 
-import './section.css';
+import { cn } from '@/lib/utils';
 
-export interface SectionProps {
-    background?: ReactNode;
-    // overlay?: ReactNode;
+export interface SectionBackgroundProps extends VideoHTMLAttributes<HTMLVideoElement> {
+    image?: string;
+    video?: string;
+    color?: string;
+    className?: string;
+    style?: CSSProperties;
 }
 
-export const Section = ({
-    background,
-    children,
-    // overlay,
-}: PropsWithChildren<SectionProps>) => {
+const SectionBackground: FC<SectionBackgroundProps> = ({
+    image,
+    video,
+    color,
+    className,
+    style,
+    ...videoProps
+}) => {
+    const baseClassNames = 'section__background absolute top-0 left-0 w-full h-full';
+
+    if (video)
+        return (
+            <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                {...videoProps}
+                style={style}
+                className={cn(baseClassNames, 'object-cover object-center', className)}
+            >
+                <source src={video} />
+                {image && (
+                    <Image
+                        src={image}
+                        alt=''
+                        fill
+                        className={cn(baseClassNames, 'object-cover object-center', className)}
+                    />
+                )}
+            </video>
+        );
+
+    if (image)
+        return (
+            <div
+                className={cn(baseClassNames, 'bg-cover bg-center', className)}
+                style={{ ...style, backgroundImage: `url(${image})` }}
+            ></div>
+        );
+
+    if (color)
+        return (
+            <div
+                className={cn(baseClassNames, 'bg-cover bg-center', `bg-${color}`, className)}
+                style={{ ...style, backgroundColor: color }}
+            ></div>
+        );
+
+    return null;
+};
+
+export interface SectionOverlayProps {
+    color?: string;
+    opacity?: number;
+    gradient?: string;
+    className?: string;
+    style?: CSSProperties;
+}
+
+const SectionOverlay: FC<SectionOverlayProps> = ({
+    color,
+    opacity,
+    gradient,
+    className,
+    style,
+}) => (
+    <div
+        className={cn`section__overlay absolute top-0 left-0 w-full h-full ${className}`}
+        style={{
+            opacity,
+            backgroundColor: gradient ? undefined : color,
+            background: gradient,
+            ...style,
+        }}
+    ></div>
+);
+
+export interface SectionContentProps {
+    className?: string;
+    children?: React.ReactNode;
+}
+
+const SectionContent: FC<SectionContentProps> = ({ className, children }) => {
     return (
-        <section className='section relative'>
-            <div className='section_background absolute inset-0'>
-                {background}
-            </div>
-            {/* <div className='section_overlay absolute inset-0 h-screen'>
-                {overlay}
-            </div> */}
-            <div className='section_content md:mx-auto container px-8 relative'>
-                {children}
-            </div>
+        <div className={cn`section__content relative container h-full mx-auto p-8 ${className}`}>
+            {children}
+        </div>
+    );
+};
+
+interface SectionProps extends HTMLAttributes<HTMLDivElement> {
+    className?: string;
+    style?: CSSProperties;
+    children?: ReactNode;
+}
+
+type SectionSubComponents = {
+    Background: typeof SectionBackground;
+    Overlay: typeof SectionOverlay;
+    Content: typeof SectionContent;
+};
+
+export const Section: FC<SectionProps> & SectionSubComponents = ({
+    className,
+    style,
+    children,
+    ...rest
+}) => {
+    return (
+        <section className={cn`section relative ${className}`} style={style} {...rest}>
+            {children}
         </section>
     );
 };
+
+Section.Background = SectionBackground;
+Section.Overlay = SectionOverlay;
+Section.Content = SectionContent;
+
+export default Section;
+
+// https://codecoolture.com/blog/react-compound-components/
+// https://github.com/codecoolture/tddworkshop.com/blob/trunk/pages/en/index.tsx
