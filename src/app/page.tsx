bdde63c976b4a44eec80d5df5c27d58e4d0d1d71
Hecker <julianheckerdev@gmail.com>
2024-06-10 00:07:53 -0400

    use navbar and button on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
-// import { Button } from '@/components/Button';
+import { Button } from '@/components/Button';
 // import { Footer } from '@/components/Footer';
-// import { Navbar } from '@/components/Navbar';
+import { Navbar } from '@/components/Navbar';
 import { Section } from '@/components/Section';
 
 export default function Home() {
     return (
         <>
-            {/* <Navbar /> */}
+            <Navbar />
             <Section className='h-screen'>
                 <Section.Background video='/assets/hero-video.mp4' />
                 <Section.Overlay className='bg-primary-900' opacity={0.5} />
                 <Section.Content className='pt-20 text-primary-100 flex flex-col align-center justify-center gap-4'>
                     <h1 className='text-5xl font-bold'>Fine Baked Goods from Wake Forest, NC</h1>
                     <p className='text-2xl'>Taste the difference of our family&apos;s recipes</p>
-                    <div className='flex flex-row gap-4'>
-                        {/* <Button href='/about' variant='outlined'>
+                    <div className='flex flex-col sm:flex-row gap-4 '>
+                        <Button href='/about' variant='outlined'>
                             About Us
                         </Button>
-                        <Button variant='solid'>Order Now!</Button> */}
+                        <Button variant='inverted'>Order Now!</Button>
                     </div>
                 </Section.Content>
             </Section>
