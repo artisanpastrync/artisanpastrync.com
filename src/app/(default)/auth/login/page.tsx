663a6bf606de5b3bd81874bbc820c7f3fbf7ef84
aaronfllr <aaronfllr@gmail.com>
2024-06-19 16:01:43 -0400

    Added login/signup, validated and it is showing on supabase along with confirmation email in my inbox

---
+++ src/app/(default)/auth/login/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/Button";
+import { login, signup } from "../_actions/login";
+
+export default function LoginPage() {
+  return (
+    <form className="space-y-8">
+      <div className="space-y-2">
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+      </div>
+      <div className="space-y-2">
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+      </div>
+      <Button formAction={login}>Log in</Button>
+      <Button formAction={signup}>Sign up</Button>
+    </form>
+  )
+}
