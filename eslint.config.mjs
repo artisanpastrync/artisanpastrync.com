686245a00cdf0b00b61856ee8f1b07f89523d37f
Julian Hecker <julianheckerdev@gmail.com>
2025-02-02 19:19:08 -0500

    update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,51 @@
+import eslintPluginNext from '@next/eslint-plugin-next';
+import prettier from 'eslint-plugin-prettier/recommended';
+import typescriptEslint from 'typescript-eslint';
+
+const ignores = [
+    '.git/',
+    '.next/',
+    'node_modules/',
+    'dist/',
+    'build/',
+    'coverage/',
+    '*.min.js',
+    '*.config.js',
+    '*.d.ts',
+];
+
+const config = typescriptEslint.config(
+    { ignores },
+    typescriptEslint.configs.recommended,
+    {
+        plugins: {
+            '@next/next': eslintPluginNext,
+        },
+        rules: {
+            ...eslintPluginNext.configs.recommended.rules,
+            ...eslintPluginNext.configs['core-web-vitals'].rules,
+        },
+    },
+    prettier,
+    {
+        settings: {
+            tailwindcss: {
+                callees: ['classnames', 'clsx', 'ctl', 'cn', 'cva'],
+            },
+        },
+        rules: {
+            '@typescript-eslint/no-empty-object-type': 'warn',
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+            'sort-imports': 'off',
+            'prettier/prettier': 'warn',
+        },
+    }
+);
+
+export default config;
