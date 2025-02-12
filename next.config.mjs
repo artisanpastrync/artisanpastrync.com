83750eb60994d7c0ec631b17a8052d953751fe01
Julian Hecker <julianheckerdev@gmail.com>
2025-02-12 15:56:46 -0500

    use authinterrupts, any image source

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        authInterrupts: true,
+    },
+    images: {
+        remotePatterns: [{ hostname: '*' }],
+    },
+};
 
 export default nextConfig;
