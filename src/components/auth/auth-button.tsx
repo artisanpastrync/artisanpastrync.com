2973b096a519f3c67b7c06429e11eb367d0d8ad0
Julian Hecker <julianheckerdev@gmail.com>
2025-02-27 23:42:14 -0500

    add cursor pointer to buttons

--- src/components/auth/auth-button.tsx
+++ src/components/auth/auth-button.tsx
@@ -1,18 +1,42 @@
 'use client';
 
+import { LogIn, LogOut } from 'lucide-react';
 import { signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
 
-import { Button } from '@/components/Button';
+import { Button, ButtonProps } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
-export function AuthButton() {
+export type AuthButtonProps = ButtonProps;
+
+export function AuthButton({ className, ...props }: AuthButtonProps) {
     const { data: session, status } = useSession();
 
-    if (status === 'loading') return <Button disabled>Sign in</Button>;
+    if (status === 'loading')
+        return (
+            <Button {...props} disabled className={cn('gap-2', className)}>
+                <LogIn />
+                Sign in
+            </Button>
+        );
 
-    if (!session?.user) return <Button href='/login'>Sign in</Button>;
+    if (!session?.user)
+        return (
+            <Button {...props} asChild className={cn('gap-2 cursor-pointer', className)}>
+                <Link href='/login'>
+                    <LogIn />
+                    Sign in
+                </Link>
+            </Button>
+        );
 
     return (
-        <Button variant='inverted' onClick={signOut}>
+        <Button
+            {...props}
+            className={cn('gap-2 cursor-pointer', className)}
+            onClick={() => signOut()}
+        >
+            <LogOut />
             Sign Out
         </Button>
     );
--- src/components/shared/theme-toggle.tsx
+++ src/components/shared/theme-toggle.tsx
@@ -4,26 +4,70 @@ import * as React from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
-import { Button } from '@/components/ui/button';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Button, ButtonProps } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
-export function ThemeToggle() {
-    const { setTheme } = useTheme();
+export type ThemeToggleProps = ButtonProps;
+
+export function ThemeToggle({ className, ...props }: ThemeToggleProps) {
+    const { setTheme, systemTheme } = useTheme();
+
+    const toggleTheme = () =>
+        setTheme((theme) => {
+            if (theme !== 'system') return 'system';
+            return systemTheme === 'dark' ? 'light' : 'dark';
+        });
 
     return (
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button variant='outline' size='icon'>
-                    <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-                    <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-                    <span className='sr-only'>Toggle theme</span>
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className='flex flex-col gap-2 p-2' align='center'>
-                <Button onClick={() => setTheme('light')}>Light</Button>
-                <Button onClick={() => setTheme('dark')}>Dark</Button>
-                <Button onClick={() => setTheme('system')}>System</Button>
-            </PopoverContent>
-        </Popover>
+        <Button
+            variant='default'
+            size='icon'
+            {...props}
+            className={cn('cursor-pointer', className)}
+            onClick={toggleTheme}
+        >
+            <Sun className='hidden dark:block' />
+            <Moon className='block dark:hidden' />
+            <span className='sr-only'>Toggle theme</span>
+        </Button>
     );
+
+    // return (
+    //     <Popover>
+    //         <PopoverTrigger asChild>
+    //             <Button variant='default' size='icon' {...props}>
+    //                 {theme === 'light' && <Sun className='h-[1.2rem] w-[1.2rem]' />}
+    //                 {theme === 'dark' && <Moon className='h-[1.2rem] w-[1.2rem]' />}
+    //                 {theme === 'system' && <SunMoon className='h-[1.2rem] w-[1.2rem]' />}
+    //                 <span className='sr-only'>Toggle theme</span>
+    //             </Button>
+    //         </PopoverTrigger>
+    //         <PopoverContent className='flex flex-col gap-2 p-2' align='center'>
+    //             <Button
+    //                 variant={theme === 'light' ? 'outline' : 'ghost'}
+    //                 onClick={() => setTheme('light')}
+    //                 className='gap-2'
+    //             >
+    //                 <Sun className='h-[1.2rem] w-[1.2rem]' />
+    //                 Light
+    //             </Button>
+    //             <Button
+    //                 variant={theme === 'dark' ? 'outline' : 'ghost'}
+    //                 onClick={() => setTheme('dark')}
+    //                 className='gap-2'
+    //             >
+    //                 <Moon className='h-[1.2rem] w-[1.2rem]' />
+    //                 Dark
+    //             </Button>
+    //             <Button
+    //                 variant={theme === 'system' ? 'outline' : 'ghost'}
+    //                 onClick={() => setTheme('system')}
+    //                 className='gap-2'
+    //             >
+    //                 <SunMoon className='h-[1.2rem] w-[1.2rem]' />
+    //                 System
+    //             </Button>
+    //         </PopoverContent>
+    //     </Popover>
+    // );
 }
