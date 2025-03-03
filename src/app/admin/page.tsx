ad8a5c93b9785cbce199c04b99d01bca70402d34
Julian Hecker <julianheckerdev@gmail.com>
2025-03-03 00:12:59 -0500

    add admin check

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,10 @@
 import { getUser } from '@/lib/auth';
+import { forbidden } from 'next/navigation';
 
 export default async function AdminPage() {
     const user = await getUser();
+    const isAdmin = user?.isAdmin;
+    if (!isAdmin) return forbidden();
+
     return <div>{JSON.stringify(user)}</div>;
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,6 @@
-import NextAuth, { type DefaultSession } from 'next-auth';
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import NextAuth from 'next-auth';
+import type { JWT as _ } from 'next-auth/jwt';
 import type { Provider } from 'next-auth/providers';
 import Google from 'next-auth/providers/google';
 
@@ -7,13 +9,31 @@ import { createStripeCustomer, getCustomerByEmail } from '@/services/customer';
 // https://authjs.dev/getting-started/typescript?framework=next-js#module-augmentation
 declare module 'next-auth' {
     /**
-     * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     * The shape of the user object returned in the OAuth providers' `profile` callback,
+     * or the second parameter of the `session` callback, when using a database.
      */
-    interface Session {
-        user: {
-            /** The user's stripe customer id. */
-            stripeCustomerId: string;
-        } & DefaultSession['user'];
+    interface User {
+        /** The user's stripe customer id. */
+        stripeCustomerId: string;
+        isAdmin: boolean;
+    }
+    /**
+     * The shape of the account object returned in the OAuth providers' `account` callback,
+     * Usually contains information about the provider being used, like OAuth tokens (`access_token`, etc).
+     */
+    interface Account {}
+
+    /**
+     * Returned by `useSession`, `auth`, contains information about the active session.
+     */
+    interface Session {}
+}
+
+declare module 'next-auth/jwt' {
+    /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+    interface JWT {
+        stripeCustomerId: string;
+        isAdmin: boolean;
     }
 }
 
@@ -22,23 +42,39 @@ const providers: Provider[] = [
 ];
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    // trustHost: process.env.NODE_ENV === 'production',
     providers,
     callbacks: {
-        async session({ session }) {
-            if (!session.user || !session.user.email) return session;
+        async jwt(params) {
+            const { token, trigger } = params;
+            if (!trigger || !token?.email) return token;
+
+            console.log('==== JWT ====');
+            console.log(JSON.stringify(params));
+
             // Find customer associated with user's email address
-            let customer = await getCustomerByEmail(session.user.email);
+            let customer = await getCustomerByEmail(token.email);
             // ...Or create a new customer
             if (!customer) {
                 customer = await createStripeCustomer({
-                    email: session.user.email,
-                    name: session.user.name ?? 'Customer',
-                    metadata: { image: session.user.image ?? null },
+                    email: token.email,
+                    name: token.name ?? 'Customer',
+                    metadata: { image: token.picture ?? null },
                 });
             }
-            // Assign customer's stripe id to user
-            session.user.stripeCustomerId = customer.id;
+
+            // todo: update stripe profile each time, as this part of the callback hits on each login
+            // await stripe.customers.update(customer.id, { metadata: {} });
+
+            token.stripeCustomerId = customer.id;
+            token.isAdmin = !!customer.metadata.isAdmin;
+
+            return token;
+        },
+        async session(params) {
+            const { session, token } = params;
+
+            session.user.stripeCustomerId = token.stripeCustomerId;
+            session.user.isAdmin = token.isAdmin;
 
             return session;
         },
